8dba5d85433e1ee0924f395ac382529ec5802161
320Jackson <jackson8963@outlook.com>
2021-10-20 12:26:02 +0800

    Add local data updater.

---
+++ Call_Update.py
@@ -0,0 +1,6 @@
+from Data_Updater import RouteInfo_Updater, RouteMap_Updater, StationInfo_Updater, StopInfo_Updater
+
+RouteInfo_Updater()
+RouteMap_Updater()
+StationInfo_Updater()
+StopInfo_Updater()
--- Data_Updater.py
+++ Data_Updater.py
@@ -18,32 +18,32 @@ def RouteInfo_Updater():
 def RouteMap_Updater():
     Path = "./RouteInfo/TPE_RouteMap.json"
     StatusCode, Buffer = Download_Task("https://ptx.transportdata.tw/MOTC/v2/Bus/StopOfRoute/City/Taipei?$format=JSON")
-    print(f"臺北市路線資訊Status:{StatusCode}")
+    print(f"臺北市站序資訊Status:{StatusCode}")
     SaveContent(Path, Buffer)
 
     Path = "./RouteInfo/NWT_RouteMap.json"
-    print(f"新北市路線資訊Status:{StatusCode}")
+    print(f"新北市站序資訊Status:{StatusCode}")
     StatusCode, Buffer = Download_Task("https://ptx.transportdata.tw/MOTC/v2/Bus/StopOfRoute/City/NewTaipei?$format=JSON")
     SaveContent(Path, Buffer)
 
 def StationInfo_Updater():
     Path = "./RouteInfo/TPE_Station.json"
     StatusCode, Buffer = Download_Task("https://ptx.transportdata.tw/MOTC/v2/Bus/Station/City/Taipei?$format=JSON")
-    print(f"臺北市路線資訊Status:{StatusCode}")
+    print(f"臺北市站位資訊Status:{StatusCode}")
     SaveContent(Path, Buffer)
 
     Path = "./RouteInfo/NWT_Station.json"
-    print(f"新北市路線資訊Status:{StatusCode}")
+    print(f"新北市站位資訊Status:{StatusCode}")
     StatusCode, Buffer = Download_Task("https://ptx.transportdata.tw/MOTC/v2/Bus/Station/City/NewTaipei?$format=JSON")
     SaveContent(Path, Buffer)
 
 def StopInfo_Updater():
     Path = "./RouteInfo/TPE_Stop.json"
     StatusCode, Buffer = Download_Task("https://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/Taipei?$format=JSON")
-    print(f"臺北市路線資訊Status:{StatusCode}")
+    print(f"臺北市站牌資訊Status:{StatusCode}")
     SaveContent(Path, Buffer)
 
     Path = "./RouteInfo/NWT_Stop.json"
-    print(f"新北市路線資訊Status:{StatusCode}")
+    print(f"新北市站牌資訊Status:{StatusCode}")
     StatusCode, Buffer = Download_Task("https://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/NewTaipei?$format=JSON")
     SaveContent(Path, Buffer)
